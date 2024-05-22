ce10df6d88383d333c95facea9acbdf52759fa9b
MLRDevs <mlrdev22@gmail.com>
2024-05-21 20:25:30 -0400

    added more projects

---
+++ Mathematics/C++/SOE/SOE.h
@@ -0,0 +1,15 @@
+// Alex Miller
+// prints all prime number to the given value
+
+#ifdef _SOE_
+#define _SOE_
+
+#include <iostream>
+#include <cstdlib>
+#include <string>
+using namespace std;
+
+int main(int argc, char** argv); // main method
+
+#endif
+
